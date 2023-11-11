47b3983199a0ceb9befc3ac12b7a6f28f04d29cd
jonaszeferino <jonaszeferino@gmail.com>
2023-11-10 21:11:35 -0300

    new index page with navbar

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,52 @@
 import Head from "next/head";
-import { ChakraProvider, Center, Box, Image } from "@chakra-ui/react";
+import { ChakraProvider, Box, Text, Center } from "@chakra-ui/react";
+import Navbar from "../components/Navbar";
 import { FaTwitter, FaYoutube, FaInstagram, FaTiktok } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <>
+    <ChakraProvider>
       <Head>
         <title>Sala de Secacao</title>
         <meta name="description" content="Site do Sala de Secacao" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <ChakraProvider>
-        <Center>
-          <h1>
-            <br />
-            <br />
-            <Center>
-              <div>Sala de Secação</div>
-            </Center>
-            <Center>
-              <div>Em Construção</div>
-            </Center>
-            <Box boxSize="sm">
-              <Image src="/sala.png" alt="logo" />
-            </Box>
-          </h1>
-        </Center>
 
-        <Center>
+      <Navbar />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <Center>
+        <Text>Site em construção</Text>
+      </Center>
+      <Center>
+        <Text>Acesse nosso conteúdo abaixo</Text>
+      </Center>
+
+      <Box pt="72px" p="20px">
+        {" "}
+        <div style={{ display: "flex", justifyContent: "center" }}>
           <a
             href="https://twitter.com/Saladesecacao"
             target="_blank"
             rel="noopener noreferrer"
-            style={{ fontSize: "50px", padding: "20px" }} // Ajuste o tamanho dos ícones aqui
+            style={{ fontSize: "50px", padding: "20px" }}
           >
             <FaTwitter />
           </a>
-          
+
           <a
             href="https://www.youtube.com/@SaladeSecacao"
             target="_blank"
             rel="noopener noreferrer"
-            style={{ fontSize: "50px", padding: "20px" }} // Ajuste o tamanho dos ícones aqui
+            style={{ fontSize: "50px", padding: "20px" }}
           >
             <FaYoutube />
           </a>
@@ -50,7 +54,7 @@ export default function Home() {
             href="https://www.instagram.com/saladesecacao/"
             target="_blank"
             rel="noopener noreferrer"
-            style={{ fontSize: "50px", padding: "20px" }} // Ajuste o tamanho dos ícones aqui
+            style={{ fontSize: "50px", padding: "20px" }}
           >
             <FaInstagram />
           </a>
@@ -58,12 +62,12 @@ export default function Home() {
             href="https://www.tiktok.com/@saladeseca?lang=pt-BR"
             target="_blank"
             rel="noopener noreferrer"
-            style={{ fontSize: "50px", padding: "20px" }} // Ajuste o tamanho dos ícones aqui
+            style={{ fontSize: "50px", padding: "20px" }}
           >
             <FaTiktok />
           </a>
-        </Center>
-      </ChakraProvider>
-    </>
+        </div>
+      </Box>
+    </ChakraProvider>
   );
 }
